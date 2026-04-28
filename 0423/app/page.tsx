0d837a2d0f4fcffccec4b9ff368d3ce6283e878a
yerin08 <hyr2663@naver.com>
2026-04-28 17:37:00 +0900

    page.tsx 수정

--- 0423/app/page.tsx
+++ 0423/app/page.tsx
@@ -5,3 +5,14 @@ export default async function RootPage() {
   const user = await getCurrentAuthUser();
   redirect(user ? '/dashboard' : '/login');
 }
+yerin good~<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Document</title>
+</head>
+<body>
+  
+</body>
+</html>
