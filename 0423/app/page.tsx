5e2be52a037f2361cede6f47f168a7270f8deb09
jihyun <albertjihyun0207@naver.com>
2026-04-29 16:34:23 +0900

    jihyun commit

--- 0423/app/page.tsx
+++ 0423/app/page.tsx
@@ -5,4 +5,14 @@ export default async function RootPage() {
   const user = await getCurrentAuthUser();
   redirect(user ? '/dashboard' : '/login');
 }
-jihyun
+jihyun<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Document</title>
+</head>
+<body>
+  
+</body>
+</html>
